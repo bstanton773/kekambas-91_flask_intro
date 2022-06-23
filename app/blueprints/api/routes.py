44cc49075aab0912f5707d0ea22bf0fea9d084b8
Brian <bstanton773@gmail.com>
2022-06-23 13:35:10 -0500

    Add a get post by post_id endpoint to api

--- app/blueprints/api/routes.py
+++ app/blueprints/api/routes.py
@@ -8,6 +8,12 @@ def index():
 
 
 @api.route('/posts')
-def posts():
+def get_posts():
     posts = Post.query.all()
     return jsonify([p.to_dict() for p in posts])
+
+
+@api.route('/posts/<post_id>')
+def get_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return jsonify(post.to_dict())
