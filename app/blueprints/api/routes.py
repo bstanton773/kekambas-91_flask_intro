13e3b27fec762b133e15813e8115382544b6afcb
Brian <bstanton773@gmail.com>
2022-06-23 12:15:27 -0500

    Add posts endpoint to get all posts, add to_dict to Post model

--- app/blueprints/api/routes.py
+++ app/blueprints/api/routes.py
@@ -1,5 +1,13 @@
 from . import bp as api
+from flask import jsonify
+from app.models import Post
 
 @api.route('/')
 def index():
     return 'Hello World'
+
+
+@api.route('/posts')
+def posts():
+    posts = Post.query.all()
+    return jsonify([p.to_dict() for p in posts])
