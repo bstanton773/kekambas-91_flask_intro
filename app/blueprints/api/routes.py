2f4c9e21bacb646caa0b9bc671a1f5cfa7b0ed06
Brian <bstanton773@gmail.com>
2022-06-23 14:49:44 -0500

    Update the Update post route to update the post, and delete route to delete the post

--- app/blueprints/api/routes.py
+++ app/blueprints/api/routes.py
@@ -41,9 +41,14 @@ def create_post():
 
 @api.route('/posts/<post_id>', methods=['PUT'])
 def update_post(post_id):
-    pass
-
+    post_to_edit = Post.query.get_or_404(post_id)
+    if not request.is_json:
+        return jsonify({'error': 'Your request content-type must be application/json'}), 400
+    post_to_edit.update(**request.json)
+    return jsonify(post_to_edit.to_dict())
 
 @api.route('/posts/<post_id>', methods=['DELETE'])
 def delete_post(post_id):
-    pass
+    post_to_delete = Post.query.get_or_404(post_id)
+    post_to_delete.delete()
+    return jsonify({'message': 'You have successfully deleted the post'})
